09b584742a4a5a25159d6d2c7bc1340cbbc37798
Krav_no0r@student.itstep.org <Krav_no0r@student.itstep.org>
2022-05-23 16:57:58 +0300

    1

---
+++ N4.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+int main()
+{
+	int a, sum1, sum2;
+	cout << "Vedite znachenie x" << endl;
+	cin >> a;
+	
+	
+
+
+	if (2<a<100)
+	{
+		int i = 0;
+		sum1 = 15 + 0 + a / pow(4, i) + pow(5, i + 2);
+		cout << "result" << sum1 << endl;
+	}
+	else
+	{
+		int j = 2;
+		10 * j * 1 - 1 / j;
+	}
+
+}
+
